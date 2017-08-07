5bc71a78b73343c2f61111e79c8e5f075ab281b1
Patrick Kim <patrick.kim408@gmail.com>
2017-08-07 13:17:28 -0800

    created time and date jsp

--- src/main/java/com/patrick/templating/HomeController.java
+++ src/main/java/com/patrick/templating/HomeController.java
@@ -1,10 +1,22 @@
 package com.patrick.templating;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
 @Controller
 public class HomeController {
 	@RequestMapping("/")
 	public String home() {
 		return "index.jsp";
+	}
+	@RequestMapping("/date")
+	public String date(Model model) {
+		model.addAttribute("dojoName", "SanJOse");
+		return "datepage.jsp";
+	}
+	@RequestMapping("/time")
+	public String time(Model model) {
+		model.addAttribute("dojoName", "Time");
+		return "timepage.jsp";
 	}
 }
